01b62a14008db5314b0d1c6a2797eb12f624b1a6
ebenamoafo <ebenamoafo222@gmail.com>
2025-11-14 10:25:32 +0000

    feat: add Counter component to track and display count

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -4,6 +4,8 @@ import Footer from './components/Footer.jsx'
 import './App.css'
 import WelcomeMessage from './components/WelcomeMessage.jsx'
 import UserProfile from './components/UserProfile.jsx'
+import Counter from './components/Counter.jsx'
+
 
 function App() {
 
@@ -12,6 +14,7 @@ function App() {
       <WelcomeMessage />
       <Header />
       <MainContent />
+      <Counter />
       <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
       <Footer />
     </>
