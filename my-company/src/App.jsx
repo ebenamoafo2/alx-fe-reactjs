8f6be4dcd3c600e96aa132f0eae7e7f500687535
ebenamoafo <ebenamoafo222@gmail.com>
2025-11-14 14:33:11 +0000

    feat(router): configure React Router with createBrowserRouter and integrate Home, About, Services, and Contact pages in App.jsx

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -1,15 +1,21 @@
-import './App.css'
-import { Link } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "./components/Home";
+import About from "./components/About";
+import Services from "./components/Services";
+import Contact from "./components/Contact";
+
 function App() {
-  
-
   return (
-    <>
-    <h1>Hello</h1>
-    
-
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
